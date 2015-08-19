0977fadb5f40551611945fc40fef52fd580d7a07
jcham01 <julien.chameroy@gmail.com>
2015-08-19 12:23:12 +0200

    correction import

--- MNVContrib/src/fr/cap/wikimnv/service/contribution/IGTag.java
+++ MNVContrib/src/fr/cap/wikimnv/service/contribution/IGTag.java
@@ -2,8 +2,6 @@ package fr.cap.wikimnv.service.contribution;
 
 import java.util.Set;
 
-import fr.cap.wikimnv.domain.pojo.Article;
-import fr.cap.wikimnv.domain.pojo.Etat;
 import fr.cap.wikimnv.domain.pojo.Tag;
 
 public interface IGTag {
