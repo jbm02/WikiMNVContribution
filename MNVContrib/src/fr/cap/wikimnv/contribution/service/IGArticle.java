78d6d14a78a8bf485f6db9fda8d72e318e485568
jbm02 <jean-baptiste.michel02@orange.fr>
2015-08-19 15:40:15 +0200

    Correction import

--- MNVContrib/src/fr/cap/wikimnv/contribution/service/IGArticle.java
+++ MNVContrib/src/fr/cap/wikimnv/contribution/service/IGArticle.java
@@ -4,7 +4,7 @@ import java.util.Set;
 
 import fr.cap.wikimnv.domain.pojo.Article;
 import fr.cap.wikimnv.domain.pojo.Etat;
-import fr.cap.wikimnv.service.global.ICRUD;
+import fr.cap.wikimnv.global.service.ICRUD;
 
 public interface IGArticle extends ICRUD {
 	void publier (Enum etat);
--- MNVContrib/src/fr/cap/wikimnv/contribution/service/IGTag.java
+++ MNVContrib/src/fr/cap/wikimnv/contribution/service/IGTag.java
@@ -3,7 +3,7 @@ package fr.cap.wikimnv.contribution.service;
 import java.util.Set;
 
 import fr.cap.wikimnv.domain.pojo.Tag;
-import fr.cap.wikimnv.service.global.ICRUD;
+import fr.cap.wikimnv.global.service.ICRUD;
 
 public interface IGTag extends ICRUD {
 	
