a65c19d8b87796245c05819e269bd9e3bfa1b75e
jcham01 <julien.chameroy@gmail.com>
2015-08-20 10:13:00 +0200

    <début impl

---
+++ MNVContrib/src/fr/cap/wikimnv/contribution/service/impl/GTagImpl.java
@@ -0,0 +1,34 @@
+package fr.cap.wikimnv.contribution.service.impl;
+
+import java.util.Set;
+
+import fr.cap.wikimnv.contribution.service.IGTag;
+import fr.cap.wikimnv.global.service.CrudImpl;
+
+public class GTagImpl extends CrudImpl implements IGTag {
+
+	@Override
+	public Set<?> lister(Class cl) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object lire(Object obj) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object supprimer(Object obj) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object sauver(Object obj) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
