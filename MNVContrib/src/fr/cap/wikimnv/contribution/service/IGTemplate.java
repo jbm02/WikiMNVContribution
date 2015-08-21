5b6da3433782880191c934b44e45b211da95dd74
javaiste <lunesec@hotmail.fr>
2015-08-21 13:53:04 +0200

    Tests Template

--- MNVContrib/src/fr/cap/wikimnv/contribution/service/IGTemplate.java
+++ MNVContrib/src/fr/cap/wikimnv/contribution/service/IGTemplate.java
@@ -1,10 +1,11 @@
 package fr.cap.wikimnv.contribution.service;
 
 import fr.cap.wikimnv.domain.pojo.Template;
+import fr.cap.wikimnv.exception.MNVException;
 import fr.cap.wikimnv.global.service.ICRUD;
 
 
 public interface IGTemplate extends ICRUD {
-	public void ajouterMeta(Template template,String metadonnee);
-	public void supprimerMeta(Template template,String metadonnee);
+	public void ajouterMeta(Template template,String metadonnee) throws MNVException;
+	public void supprimerMeta(Template template,String metadonnee) throws MNVException;
 }
