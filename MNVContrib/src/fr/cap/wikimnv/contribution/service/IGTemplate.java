c3835bba477604dfe7200df5bae010ebdf574640
javaiste <lunesec@hotmail.fr>
2015-08-21 14:11:07 +0200

    Mise en place des commentaires pour Template

--- MNVContrib/src/fr/cap/wikimnv/contribution/service/IGTemplate.java
+++ MNVContrib/src/fr/cap/wikimnv/contribution/service/IGTemplate.java
@@ -3,9 +3,21 @@ package fr.cap.wikimnv.contribution.service;
 import fr.cap.wikimnv.domain.pojo.Template;
 import fr.cap.wikimnv.exception.MNVException;
 import fr.cap.wikimnv.global.service.ICRUD;
+import fr.cap.wikimnv.global.service.ICRUDGeneric;
 
 
-public interface IGTemplate extends ICRUD {
+public interface IGTemplate extends ICRUDGeneric {
+	/**
+	 * elle permet d'ajouter une metadonnee à mon template
+	 * @param template
+	 * @param metadonnee
+	 */
 	public void ajouterMeta(Template template,String metadonnee) throws MNVException;
+	
+	/**
+	 * elle permet de supprimer une metadonnee de mon template
+	 * @param template
+	 * @param metadonnee
+	 */
 	public void supprimerMeta(Template template,String metadonnee) throws MNVException;
 }
