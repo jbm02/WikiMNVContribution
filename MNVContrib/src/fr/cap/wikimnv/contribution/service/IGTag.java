76d9df1e1aeb366b58b03807fd8ab84d642c6ab4
jbm02 <jean-baptiste.michel02@orange.fr>
2015-08-21 11:45:42 +0200

    test merge

--- MNVContrib/src/fr/cap/wikimnv/contribution/service/IGTag.java
+++ MNVContrib/src/fr/cap/wikimnv/contribution/service/IGTag.java
@@ -5,6 +5,15 @@ import java.util.Set;
 import fr.cap.wikimnv.domain.pojo.Tag;
 import fr.cap.wikimnv.global.service.ICRUD;
 
-public interface IGTag extends ICRUD {
+
+public interface IGTag extends ICRUD{
 	
+	/**
+	 * Liste les tags contenant le texte sélectionné.
+	 * @param setTag
+	 * @param libelle
+	 * @return
+	 */
+	Set<Tag> rechercheTextuelle(String libelle);
 }
+
--- MNVContrib/src/fr/cap/wikimnv/contribution/service/impl/GTagImpl.java
+++ MNVContrib/src/fr/cap/wikimnv/contribution/service/impl/GTagImpl.java
@@ -3,31 +3,37 @@ package fr.cap.wikimnv.contribution.service.impl;
 import java.util.Set;
 
 import fr.cap.wikimnv.contribution.service.IGTag;
-import fr.cap.wikimnv.global.service.CrudImpl;
+import fr.cap.wikimnv.domain.pojo.Query;
+import fr.cap.wikimnv.domain.pojo.Tag;
+import fr.cap.wikimnv.global.service.ICRUD;
+import fr.cap.wikimnv.global.service.impl.CrudImpl;
 
-public class GTagImpl extends CrudImpl implements IGTag {
 
+public class GTagImpl implements IGTag {
+	ICRUD crud = new CrudImpl();
+	
 	@Override
-	public Set<?> lister(Class cl) {
-		// TODO Auto-generated method stub
-		return null;
+	public Set<?> lister(Class cls) {
+		return crud.lister(Tag.class);
 	}
 
 	@Override
-	public Object lire(Object obj) {
-		// TODO Auto-generated method stub
-		return null;
+	public Object lire(Object obj, Class cls) {
+		return crud.lire(obj, Tag.class);
 	}
 
 	@Override
 	public Object supprimer(Object obj) {
-		// TODO Auto-generated method stub
-		return null;
+		return crud.supprimer(obj);
 	}
 
 	@Override
 	public Object sauver(Object obj) {
-		// TODO Auto-generated method stub
+		return crud.sauver(obj);
+	}
+
+	@Override
+	public Set<Tag> rechercheTextuelle(String libelle) {
 		return null;
 	}
 
