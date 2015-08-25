423b366ee1d9a11245c4f29267d44fd7ce4c0b5f
jbm02 <jean-baptiste.michel02@orange.fr>
2015-08-25 16:17:04 +0200

    LoggerMNV

---
+++ MNVContrib/src/fr/cap/wikimnv/contribution/aspect/LoggerMNV.java
@@ -0,0 +1,34 @@
+package fr.cap.wikimnv.contribution.aspect;
+
+import java.util.Calendar;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import fr.cap.wikimnv.exception.MNVException;
+
+public class LoggerMNV {
+
+	Logger logger = LoggerFactory.getLogger(LoggerMNV.class);
+	
+	public void logEvent(JoinPoint joinPoint){
+		logger.info(joinPoint.getThis().getClass().getSimpleName(), joinPoint.getArgs().length);
+	}
+	
+	public Object logProcess(ProceedingJoinPoint joinPoint){
+		Object returnValue = null;
+		
+		try {
+			logger.info(Calendar.getInstance().getTimeInMillis() + " - Start " + joinPoint.getSignature().toString());
+			returnValue = joinPoint.proceed();
+			logger.info(Calendar.getInstance().getTimeInMillis() + " - End " + joinPoint.getSignature().toString());
+		} catch (MNVException e){	
+			logger.debug(e.getCodeErreur() + " : " + e.getMessage());
+		} catch (Throwable e) {
+			logger.debug("Erreur non traitée" + " : " + joinPoint.getSignature().toString() + " - " + e.getMessage());
+		}
+		return returnValue;
+	}
+}
