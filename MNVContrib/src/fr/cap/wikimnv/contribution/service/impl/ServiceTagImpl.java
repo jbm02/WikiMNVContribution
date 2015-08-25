d34294d1b5c6a81531766827b29a0c41ba8f78fb
jbm02 <jean-baptiste.michel02@orange.fr>
2015-08-25 11:02:46 +0200

    Ajout Tag

--- MNVContrib/src/fr/cap/wikimnv/contribution/service/impl/ServiceTagImpl.java
+++ MNVContrib/src/fr/cap/wikimnv/contribution/service/impl/ServiceTagImpl.java
@@ -4,42 +4,54 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import fr.cap.wikimnv.contribution.service.IServiceTag;
 import fr.cap.wikimnv.exception.MNVException;
 import fr.cap.wikimnv.global.domain.pojo.Query;
 import fr.cap.wikimnv.global.domain.pojo.Tag;
 import fr.cap.wikimnv.global.service.ICRUD;
-import fr.cap.wikimnv.global.service.impl.CrudImplBouchon;
 
+@Service("sTag")
 public class ServiceTagImpl implements IServiceTag {
-
-	ICRUD crud = new CrudImplBouchon();
 	
+	@Autowired
+	ICRUD sCrud;
+	
+	public ICRUD getsCrud() {
+		return sCrud;
+	}
+
+	public void setsCrud(ICRUD sCrud) {
+		this.sCrud = sCrud;
+	}
+
 	@Override
 	public Set<?> lister(Class cls) throws MNVException {
-		return crud.lister(Tag.class);
+		return sCrud.lister(Tag.class);
 	}
 
 	@Override
 	public Object lire(Object obj, Class cls) throws MNVException {
-		return crud.lire(obj, Tag.class);
+		return sCrud.lire(obj, Tag.class);
 	}
 
 	@Override
 	public Object supprimer(Object obj) throws MNVException {
-		return crud.supprimer(obj);
+		return sCrud.supprimer(obj);
 	}
 
 	@Override
 	public Object sauver(Object obj) throws MNVException {
-		return crud.sauver(obj);
+		return sCrud.sauver(obj);
 	}
 
 	@Override
 	public Set<Tag> rechercheTextuelle(String libelle) throws MNVException {
 		Map<String, Object> params = new HashMap<String, Object>();
 		params.put("pTexte", libelle);
-		return (Set<Tag>) crud.faireRequete(Query.TAG_RECHERCHETEXTUELLE, params);
+		return (Set<Tag>) sCrud.faireRequete(Query.TAG_RECHERCHETEXTUELLE, params);
 		
 	}
 	
--- MNVContrib/src/fr/cap/wikimnv/test/contribution/service/IServiceTagTest.java
+++ MNVContrib/src/fr/cap/wikimnv/test/contribution/service/IServiceTagTest.java
@@ -1,26 +1,31 @@
 package fr.cap.wikimnv.test.contribution.service;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.fail;
 
 import java.util.HashSet;
 import java.util.Set;
 
 import org.junit.Test;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import fr.cap.wikimnv.contribution.service.IServiceTag;
-import fr.cap.wikimnv.contribution.service.impl.ServiceTagImpl;
 import fr.cap.wikimnv.exception.MNVException;
 import fr.cap.wikimnv.global.domain.pojo.Tag;
 
 public class IServiceTagTest {
 	
-	IServiceTag gtag = new ServiceTagImpl();
+	IServiceTag serviceTag;
+	
+	public IServiceTagTest() {
+		serviceTag = (IServiceTag) new ClassPathXmlApplicationContext("applicationContext.xml").getBean("sTag");
+	}
+	
 	String libelle = "test";
 	Set<Tag> listetag = new HashSet<Tag>();
 	@Test
 	public void testRechercheTextuelle() {
 		try {
-			listetag = gtag.rechercheTextuelle(libelle);
+			listetag = serviceTag.rechercheTextuelle(libelle);
 		} catch (MNVException e) {
 			fail("Not yet implemented");
 		}
