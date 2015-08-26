36c6f599e20886bb224c6d0212201d02b346d853
jbm02 <jean-baptiste.michel02@orange.fr>
2015-08-26 10:12:33 +0200

    modif pattern log4j

--- MNVContrib/src/fr/cap/wikimnv/contribution/service/impl/ServiceTemplateImpl.java
+++ MNVContrib/src/fr/cap/wikimnv/contribution/service/impl/ServiceTemplateImpl.java
@@ -1,13 +1,14 @@
 package fr.cap.wikimnv.contribution.service.impl;
 
+import java.util.Set;
+
 import fr.cap.wikimnv.contribution.service.IServiceTemplate;
 import fr.cap.wikimnv.exception.MNVException;
 import fr.cap.wikimnv.global.domain.pojo.Template;
 import fr.cap.wikimnv.global.service.ICRUD;
-import fr.cap.wikimnv.global.service.impl.CrudImpl;
 
 
-public class ServiceTemplateImpl extends CrudImpl implements IServiceTemplate {
+public class ServiceTemplateImpl implements IServiceTemplate {
 	
 	ICRUD sCrud;
 	
@@ -29,6 +30,30 @@ public class ServiceTemplateImpl extends CrudImpl implements IServiceTemplate {
 	public void supprimerMeta(Template template, String metadonnee) throws MNVException{
 		template.getMeta().remove(metadonnee); 
 		this.sauver(template);
+	}
+
+	@Override
+	public Object lire(Object obj, Class cls) throws MNVException {
+		// TODO Auto-generated method stub
+		return sCrud.lire(obj, Template.class);
+	}
+
+	@Override
+	public Set<?> lister(Class cls) throws MNVException {
+		// TODO Auto-generated method stub
+		return sCrud.lister(Template.class);
+	}
+
+	@Override
+	public Object sauver(Object obj) throws MNVException {
+		// TODO Auto-generated method stub
+		return sCrud.sauver(obj);
+	}
+
+	@Override
+	public Object supprimer(Object obj) throws MNVException {
+		// TODO Auto-generated method stub
+		return sCrud.supprimer(obj);
 	}
 
 }
